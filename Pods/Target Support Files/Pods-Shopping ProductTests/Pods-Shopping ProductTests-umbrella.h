68ff30b82362ba611a22b55fec207bbe020e40da
chetanankola <chetan.ankola@gmail.com>
2016-05-26 11:07:51 -0700

    removing pods

--- Pods/Target Support Files/Pods-Shopping ProductTests/Pods-Shopping ProductTests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_Shopping_ProductTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Shopping_ProductTestsVersionString[];
-
--- Pods/Target Support Files/Pods-Shopping ProductUITests/Pods-Shopping ProductUITests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_Shopping_ProductUITestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Shopping_ProductUITestsVersionString[];
-
