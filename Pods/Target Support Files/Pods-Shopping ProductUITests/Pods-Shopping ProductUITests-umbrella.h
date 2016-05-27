ca7bbc3e81e703f6dc319f57df549506d2a38a46
chetanankola <chetan.ankola@gmail.com>
2016-05-27 02:15:52 -0700

    gitignore

---
+++ Pods/Target Support Files/Pods-Shopping ProductUITests/Pods-Shopping ProductUITests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Shopping_ProductUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Shopping_ProductUITestsVersionString[];
+
