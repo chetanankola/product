810a5a1267ac1c72f5b8481f0669c058e65682ab
chetanankola <chetan.ankola@gmail.com>
2016-05-27 02:18:18 -0700

    removing unnecesssary files

--- Pods/Target Support Files/Pods-Shopping Product/Pods-Shopping Product-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_Shopping_ProductVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Shopping_ProductVersionString[];
-
--- Pods/Target Support Files/Pods-Shopping ProductUITests/Pods-Shopping ProductUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Shopping_ProductUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Shopping_ProductUITests
-@end
