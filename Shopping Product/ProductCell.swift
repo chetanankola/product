2fab192d643e64b57c0282531dac784e2654ae94
chetanankola <chetan.ankola@gmail.com>
2016-05-31 00:14:01 -0700

    layout adjustment

--- Shopping Product/ProductCell.swift
+++ Shopping Product/ProductCell.swift
@@ -11,9 +11,9 @@ import UIKit
 
 class ProductCell: UICollectionViewCell {
 
+    @IBOutlet weak var Price: UILabel!
     @IBOutlet weak var ProductName: UILabel!
     @IBOutlet weak var CardView: UIView!
-    @IBOutlet weak var ProductShortDescription: UILabel!
     @IBOutlet weak var ProductImage: UIImageView!
     var product:Product? {
         didSet {
@@ -21,7 +21,9 @@ class ProductCell: UICollectionViewCell {
         }
     }
     override func prepareForReuse() {
+        ProductImage.image = nil
         super.prepareForReuse()
+        
     }
     
     func populateData() {
@@ -30,22 +32,28 @@ class ProductCell: UICollectionViewCell {
         if let productImage = product?.productImage {
             //--Product Image---//
             ProductImage.loadImageFromURLString(productImage, placeholderImage: AppConstants.BLUR_PLACEHOLDER_IMAGE, completion: nil)
+            
+            
         }
         
+        
+        if let price = product?.price {
+            Price.text = price
+        }
         
         if let productName = product?.productName {
             ProductName.text = productName
         }
 
-        if let productShortTitle = product?.shortDescription {
-            //---Short Description//
-            let str = productShortTitle.stringByReplacingOccurrencesOfString("<.*?>", withString: "", options: .RegularExpressionSearch, range: nil)
-            let attrStr = try! NSAttributedString(
-                data:  str.dataUsingEncoding(NSUnicodeStringEncoding, allowLossyConversion: true)!,
-                options: [ NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType],
-                documentAttributes: nil)
-            ProductShortDescription.attributedText = attrStr
-        }
+//        if let productShortTitle = product?.shortDescription {
+//            //---Short Description//
+//            let str = productShortTitle.stringByReplacingOccurrencesOfString("<.*?>", withString: "", options: .RegularExpressionSearch, range: nil)
+//            let attrStr = try! NSAttributedString(
+//                data:  str.dataUsingEncoding(NSUnicodeStringEncoding, allowLossyConversion: true)!,
+//                options: [ NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType],
+//                documentAttributes: nil)
+//            ProductShortDescription.attributedText = attrStr
+//        }
         
         
         
